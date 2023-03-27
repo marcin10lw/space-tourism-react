0c18ca2bdc1610b800843780c75d624ff42e2694
marcin10lw <marcin10lw@wp.pl>
2023-03-27 23:27:49 +0200

    setup theme.js and GlobalStyle.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, ::before, ::after {
+    box-sizing: inherit;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    vulcan: "#0B0D17",
+    blue: "#D0D6F9",
+    white: "#FFFFFF",
+  },
+};
