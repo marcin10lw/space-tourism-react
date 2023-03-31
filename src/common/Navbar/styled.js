c698330b74cc511de757641cd7d32d1c4a8ffc78
marcin10lw <marcin10lw@wp.pl>
2023-04-01 01:21:50 +0200

    styled Navbar for desktop

--- src/common/Navbar/styled.js
+++ src/common/Navbar/styled.js
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
+const white = ({ theme }) => theme.colors.white;
+
 export const StyledNavbar = styled.nav`
-  height: 96px;
   display: flex;
   align-items: center;
+  justify-content: center;
   background: rgba(255, 255, 255, 0.04);
   backdrop-filter: blur(40.7742px);
 `;
@@ -18,9 +20,7 @@ export const LinksList = styled.ul`
   gap: 48px;
 `;
 
-export const ListElement = styled.li`
-  height: 100%;
-`;
+export const ListElement = styled.li``;
 
 export const Number = styled.span`
   margin-right: 12px;
@@ -28,7 +28,39 @@ export const Number = styled.span`
 `;
 
 export const StyledNavLink = styled(NavLink)`
+  display: block;
+  padding: 38px 0;
   color: inherit;
   text-decoration: none;
   letter-spacing: 2.7px;
+  position: relative;
+
+  &:hover {
+    &:before {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 3px;
+      background-color: ${white};
+      opacity: 0.5;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      z-index: 1;
+    }
+  }
+
+  &.active {
+    &:after {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 3px;
+      background-color: ${white};
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      z-index: 100;
+    }
+  }
 `;
