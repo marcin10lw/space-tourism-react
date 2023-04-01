30b4638a512e1e3a3c4abd1618218ee247279986
marcin10lw <marcin10lw@wp.pl>
2023-04-01 16:38:43 +0200

    styled Navbar for mobile view

--- src/common/Navbar/index.js
+++ src/common/Navbar/index.js
@@ -1,44 +1,53 @@
+import { useState } from "react";
 import {
   LinksList,
   ListElement,
   Number,
   StyledNavbar,
   StyledNavLink,
+  OpenMenuButton,
+  CloseMenuButton,
 } from "./styled";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <StyledNavbar>
-      <LinksList>
-        <ListElement>
-          <StyledNavLink to="/home">
-            <Number>00</Number>
-            HOME
-          </StyledNavLink>
-        </ListElement>
+    <>
+      <StyledNavbar isMenuOpen={isMenuOpen}>
+        <LinksList>
+          <ListElement>
+            <StyledNavLink to="/home">
+              <Number>00</Number>
+              HOME
+            </StyledNavLink>
+          </ListElement>
 
-        <ListElement>
-          <StyledNavLink to="/destination">
-            <Number>01</Number>
-            DESTINATION
-          </StyledNavLink>
-        </ListElement>
+          <ListElement>
+            <StyledNavLink to="/destination">
+              <Number>01</Number>
+              DESTINATION
+            </StyledNavLink>
+          </ListElement>
 
-        <ListElement>
-          <StyledNavLink to="/crew">
-            <Number>00</Number>
-            CREW
-          </StyledNavLink>
-        </ListElement>
+          <ListElement>
+            <StyledNavLink to="/crew">
+              <Number>00</Number>
+              CREW
+            </StyledNavLink>
+          </ListElement>
 
-        <ListElement>
-          <StyledNavLink to="/technology">
-            <Number>00</Number>
-            TECHNOLOGY
-          </StyledNavLink>
-        </ListElement>
-      </LinksList>
-    </StyledNavbar>
+          <ListElement>
+            <StyledNavLink to="/technology">
+              <Number>00</Number>
+              TECHNOLOGY
+            </StyledNavLink>
+          </ListElement>
+        </LinksList>
+        <CloseMenuButton onClick={() => setIsMenuOpen(false)} />
+      </StyledNavbar>
+      <OpenMenuButton onClick={() => setIsMenuOpen(true)} />
+    </>
   );
 };
 
--- src/common/Navbar/styled.js
+++ src/common/Navbar/styled.js
@@ -1,9 +1,7 @@
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
-
-const white = ({ theme }) => theme.colors.white;
-const bpMobile = ({ theme }) => theme.breakpoints.mobile;
-const bpTablet = ({ theme }) => theme.breakpoints.tablet;
+import styled, { css } from "styled-components";
+import hamburgerIcon from "../../assets/shared/icon-hamburger.svg";
+import closeIcon from "../../assets/shared/icon-close.svg";
 
 export const StyledNavbar = styled.nav`
   flex-basis: 50%;
@@ -12,6 +10,53 @@ export const StyledNavbar = styled.nav`
   justify-content: center;
   background: rgba(255, 255, 255, 0.04);
   backdrop-filter: blur(40.7742px);
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    display: block;
+    position: fixed;
+    z-index: 999;
+    width: 68%;
+    top: 0;
+    bottom: 0;
+    right: -100%;
+    transition: right 300ms ease-in-out;
+
+    ${({ isMenuOpen }) =>
+      isMenuOpen &&
+      css`
+        right: 0;
+      `}
+  }
+`;
+
+export const OpenMenuButton = styled.button`
+  display: none;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    display: block;
+    width: 24px;
+    height: 21px;
+    border: none;
+    background: center / cover url(${hamburgerIcon});
+    margin-right: 24px;
+    cursor: pointer;
+  }
+`;
+
+export const CloseMenuButton = styled.button`
+  display: none;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    display: block;
+    width: 19px;
+    height: 19px;
+    border: none;
+    background: center / cover url(${closeIcon});
+    position: absolute;
+    top: 34px;
+    right: 26px;
+    cursor: pointer;
+  }
 `;
 
 export const LinksList = styled.ul`
@@ -22,20 +67,35 @@ export const LinksList = styled.ul`
   align-items: center;
   gap: 48px;
 
-  @media (max-width: ${bpTablet}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
     margin: 0 48px;
     gap: 37px;
   }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 113px 0 0;
+    gap: 20px;
+  }
 `;
 
-export const ListElement = styled.li``;
+export const ListElement = styled.li`
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 100%;
+  }
+`;
 
 export const Number = styled.span`
   margin-right: 12px;
   font-weight: 700;
 
-  @media (max-width: ${bpTablet}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
     display: none;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    display: inline-block;
   }
 `;
 
@@ -53,12 +113,16 @@ export const StyledNavLink = styled(NavLink)`
       display: block;
       width: 100%;
       height: 3px;
-      background-color: ${white};
+      background-color: ${({ theme }) => theme.colors.white};
       opacity: 0.5;
       position: absolute;
       left: 0;
       bottom: 0;
       z-index: 1;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        display: none;
+      }
     }
   }
 
@@ -68,16 +132,29 @@ export const StyledNavLink = styled(NavLink)`
       display: block;
       width: 100%;
       height: 3px;
-      background-color: ${white};
+      background-color: ${({ theme }) => theme.colors.white};
       position: absolute;
       left: 0;
       bottom: 0;
       z-index: 100;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        width: 4px;
+        height: 100%;
+        right: 0;
+        left: auto;
+      }
     }
   }
 
-  @media (max-width: ${bpTablet}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
     letter-spacing: 2.36px;
     font-size: 14px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    letter-spacing: 2.7px;
+    font-size: 16px;
+    padding: 6px 0 6px 32px;
   }
 `;
