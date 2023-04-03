79500a60d0bf895ce449610bd53f7611737c37c0
marcin10lw <marcin10lw@wp.pl>
2023-04-03 23:23:18 +0200

    setup framer motion pages animation

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   return (
     <>
-      <GlobalStyle path={pathname} />
+      <GlobalStyle path={location.pathname} />
       <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,6 +4,7 @@ import { Button, HomeArticle, HomeSection, HomeTitle } from "./styled";
 import Description from "../../components/Description";
 import description from "./description";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -14,7 +15,12 @@ const HomePage = () => {
 
   return (
     <Main>
-      <HomeSection>
+      <HomeSection
+        as={motion.section}
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ y: window.innerWidth, transition: { duration: 0.4 } }}
+      >
         <HomeArticle>
           <Heading content="so, you want to travel to" />
           <HomeTitle>SPACE</HomeTitle>
