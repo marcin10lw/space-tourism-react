556c62540d99eeb8765391c6ff6e9667d3b17899
marcin10lw <marcin10lw@wp.pl>
2023-04-03 22:09:29 +0200

    created Heading component

---
+++ src/components/Heading/index.js
@@ -0,0 +1,12 @@
+import { StyledHeading, HeadingNumber } from "./styled";
+
+const Heading = ({ number, content }) => {
+  return (
+    <StyledHeading>
+      {number && <HeadingNumber>{number}</HeadingNumber>}
+      {content.toUpperCase()}
+    </StyledHeading>
+  );
+};
+
+export default Heading;
---
+++ src/components/Heading/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledHeading = styled.h1`
+  margin: 0;
+  font-size: 28px;
+  font-weight: 400;
+  letter-spacing: 4.72px;
+  color: ${({ theme }) => theme.colors.blue};
+`;
+
+export const HeadingNumber = styled.span``;
