b9bba4585af60f09724adf7aa307f875cb4aa842
marcin10lw <marcin10lw@wp.pl>
2023-04-03 20:38:41 +0200

    gave background images for mobile and tablet views

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,19 @@
 import { createGlobalStyle, css } from "styled-components";
 import homeBackgroundDesktop from "./assets/home/background-home-desktop.jpg";
+import homeBackgroundTablet from "./assets/home/background-home-tablet.jpg";
+import homeBackgroundMobile from "./assets/home/background-home-mobile.jpg";
+
 import destinationBackgroundDesktop from "./assets/destination/background-destination-desktop.jpg";
+import destinationBackgroundTablet from "./assets/destination/background-destination-tablet.jpg";
+import destinationBackgroundMobile from "./assets/destination/background-destination-mobile.jpg";
+
 import crewBackgroundDesktop from "./assets/crew/background-crew-desktop.jpg";
+import crewBackgroundTablet from "./assets/crew/background-crew-tablet.jpg";
+import crewBackgroundMobile from "./assets/crew/background-crew-mobile.jpg";
+
 import technologyBackgroundDesktop from "./assets/technology/background-technology-desktop.jpg";
+import technologyBackgroundTablet from "./assets/technology/background-technology-tablet.jpg";
+import technologyBackgroundMobile from "./assets/technology/background-technology-mobile.jpg";
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -27,23 +38,58 @@ const GlobalStyle = createGlobalStyle`
       path === "/home" &&
       css`
         background: no-repeat fixed center / cover url(${homeBackgroundDesktop});
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+          background-image: url(${homeBackgroundTablet});
+        }
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+          background-image: url(${homeBackgroundMobile});
+        } ;
       `}
+
     ${({ path }) =>
       path === "/destination" &&
       css`
         background: no-repeat fixed center / cover
           url(${destinationBackgroundDesktop});
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+          background-image: url(${destinationBackgroundTablet});
+        }
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+          background-image: url(${destinationBackgroundMobile});
+        } ;
       `}
+
     ${({ path }) =>
       path === "/crew" &&
       css`
         background: no-repeat fixed center / cover url(${crewBackgroundDesktop});
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+          background-image: url(${crewBackgroundTablet});
+        }
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+          background-image: url(${crewBackgroundMobile});
+        } ;
       `}
+
     ${({ path }) =>
       path === "/technology" &&
       css`
         background: no-repeat fixed center / cover
           url(${technologyBackgroundDesktop});
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+          background-image: url(${technologyBackgroundTablet});
+        }
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+          background-image: url(${technologyBackgroundMobile});
+        } ;
       `}
   }
 `;
