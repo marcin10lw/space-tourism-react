ac72dfb8552b0d5df422f5ed3ab699876f1f4cf5
marcin10lw <marcin10lw@wp.pl>
2023-04-04 23:19:07 +0200

    made Description into styled component

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -1,7 +1,19 @@
-import { StyledDescription } from "./styled";
+import styled from "styled-components";
 
-const Description = ({ description }) => {
-  return <StyledDescription>{description}</StyledDescription>;
-};
+export const Description = styled.p`
+  margin: 0;
+  font-family: "Barlow", sans-serif;
+  font-size: 18px;
+  line-height: 32px;
+  color: ${({ theme }) => theme.colors.blue};
 
-export default Description;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    font-size: 16px;
+    line-height: 28px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 15px;
+    line-height: 28px;
+  }
+`;
