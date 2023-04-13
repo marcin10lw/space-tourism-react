3c677f1d8253bdecf8a3d83f53f23329b59f9af2
marcin10lw <marcin10lw@wp.pl>
2023-04-13 21:07:44 +0200

    adjusted HomePage animation

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -17,11 +17,11 @@ const HomePage = () => {
     <Main
       as={motion.main}
       initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}
+      animate={{ opacity: 1, x: 0, transition: { duration: 0.2, delay: 0.2 } }}
       exit={{
         x: -100,
         opacity: 0,
-        transition: { duration: 0.1 },
+        transition: { duration: 0.2 },
       }}
     >
       <HomeSection>
